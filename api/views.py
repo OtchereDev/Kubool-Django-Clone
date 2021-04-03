665ececb11c4a68e79b6cf45e7bd65280bcd5152
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-03 00:05:37 +0000

    added view for checking user exists and for  getting all anon messages

--- api/views.py
+++ api/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated
+from api.models import CustomUser
+from .serializers import MessageSerializer
+
 from rest_framework.views import APIView
+from rest_framework.exceptions import NotFound, ParseError
+from rest_framework.response import Response
+
+from django.contrib.auth.password_validation import validate_password
 
 
-# class
+class CheckUser(APIView):
+    """ view for verifying if user or not """
+    def post(self,request,*args, **kwargs):
+        share_code=kwargs['share_code']
+        try:
+            user=CustomUser.objects.get(share_code=share_code)
+        except CustomUser.DoesNotExist:
+            raise NotFound(detail="Sorry not user with this user code exits")
+        
+        return Response({'status':'user exists'})
 
+
+
+class AllAnonMessages(APIView):
+    """ Return all Anonymous messages """
+    permission_classes=[IsAuthenticated,]
+    def get(self,request,*args, **kwargs):
+        user=request.user
+        messages=MessageSerializer(user.messages.all(),many=True)
+
+        return Response({'messages':messages.data})
