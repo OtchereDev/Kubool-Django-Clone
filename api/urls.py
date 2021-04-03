9abf8ae92d40307dfacef6ca447deac0860d4f32
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-03 01:16:39 +0000

    added endpoints for signup,change password,delete user and check user

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,12 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from .views import (CheckUser,
+                    AllAnonMessages,
+                    SignUp,
+                    ChangePassword,
+                    DeleteUser)
+
 app_name='api'
 
 urlpatterns = [
@@ -12,6 +18,11 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('user/messages/',AllAnonMessages.as_view(),name='all_messages'),
+    path('user/signup/',SignUp.as_view(),name="sign_up"),
+    path('user/change_password/',ChangePassword.as_view(),name='change_password'),
+    path('user/delete/',DeleteUser.as_view(),name='delete_user'),
+    path('user/<str:share_code>/',CheckUser.as_view(),name='check_user'),
     
 ]
 
