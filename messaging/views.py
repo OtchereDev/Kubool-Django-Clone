6a0ff67852a4bf7e74155a110595305b7ece332a
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-03 02:34:09 +0000

    added celery task func to view for sending anon message

--- messaging/views.py
+++ messaging/views.py
@@ -1,3 +1,4 @@
+from messaging.tasks import deleteMessageTask
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError,NotFound
@@ -5,6 +6,8 @@ from rest_framework.permissions import AllowAny
 
 from messaging.models import Message
 from api.models import CustomUser
+
+from datetime import timedelta
 
 
 # for accepting new messages 
@@ -21,6 +24,8 @@ class NewMessage(APIView):
             new_message=Message.objects.create(content=message)
 
             user.messages.add(new_message)
+
+            deleteMessageTask.apply_async((new_message.id,),eta=new_message.created+timedelta(minutes=5))
             
         except KeyError:
             raise ParseError(detail='You must provide a message in the key "message"')
