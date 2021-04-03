18c136404ea41890cc0e5e386d941695cad04650
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-03 01:19:14 +0000

    included app level urls

--- kubool_clone/urls.py
+++ kubool_clone/urls.py
@@ -1,9 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path,include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('api.urls',namespace='api'))
+    path('api/',include('api.urls',namespace='api')),
+    path('',include('messaging.urls',namespace='messaging')),
 ]
