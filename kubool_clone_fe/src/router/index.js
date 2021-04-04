04feeb6d23994829e01ee2fa3ac3ec4ea65792a2
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-04 18:38:42 +0000

    setup differnt routes for application

--- kubool_clone_fe/src/router/index.js
+++ kubool_clone_fe/src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Auth from '../views/Auth.vue'
+import Settings from '../views/Settings.vue'
+import ChangePassword from '../views/ChangePassword.vue'
+import Disclaimer from '../views/Disclaimer.vue'
+import Contact from '../views/Contact.vue'
+
 
 const routes = [
   {
@@ -7,14 +13,31 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+ {
+   path:'/auth',
+   name:'Auth',
+   component:Auth
+ },
+ {
+   path:'/settings',
+   name:'Settings',
+   component:Settings
+ },
+ {
+   path:'/settings/change_password',
+   name:'Change_password',
+   component:ChangePassword
+ },
+ {
+  path:'/settings/disclaimer',
+  name:'Disclaimer',
+  component:Disclaimer
+},
+{
+  path:'/contact',
+  name:'Contact',
+  component:Contact
+}
 ]
 
 const router = createRouter({
