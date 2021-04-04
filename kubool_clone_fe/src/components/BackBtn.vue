4191e0f00993a0467cbdbd8ed94cea3b9bdb9782
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-04 18:39:45 +0000

    added a resuable style component for going back

---
+++ kubool_clone_fe/src/components/BackBtn.vue
@@ -0,0 +1,19 @@
+<template>
+    <hr style="border-color:#a64bf4;" class="border my-10">
+
+    <div class="text-center my-4">
+        <button style="border-radius:32px;" class="flex w-full rounded-3xl py-4 text-center justify-center items-center bg-custome_bg text-gray-100 text-lg focus:outline-none">
+            <ion-icon name="arrow-back-outline" class="text-2xl font-medium" ></ion-icon><span class="ml-2 font-medium">Go Back</span>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
