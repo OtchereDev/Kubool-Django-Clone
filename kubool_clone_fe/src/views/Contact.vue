98ac59a10a1e26a1b2429e9887b7668120ba29d9
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-04 18:36:42 +0000

    template done : Contact me page for developer

---
+++ kubool_clone_fe/src/views/Contact.vue
@@ -0,0 +1,44 @@
+<template>
+      <div class="min-h-screen bg-custome_bg bg-cover bg-no-repeat py-20 font-custom_font">
+            <div class="mx-auto w-11/12 md:w-5/12 bg-custom_blue py-20 rounded-xl px-16">
+
+                <h2 class="text-center  font-bold text-gray-50 text-4xl tracking-wide">
+                    Contact Me
+                </h2>
+                <p class="text-base text-gray-300 my-5">
+
+                    You can contact me using the following social media platforms listed down below:
+                </p>
+
+                <p class="text-base text-gray-300 my-5">
+
+                    Twitter: <a href="https://twitter.com/code_otchere" class="font-semibold">@code_otchere</a>
+                </p>
+                <p class="text-base text-gray-300 my-5">
+
+                    LinkedIn: <a href="https://linkedin.com/in/oliverotchere" class="font-semibold">@Oliver Otchere</a>
+                </p>
+                <p class="text-base text-gray-300 my-5">
+
+                    GitHub: <a href="https://github.com/OtchereDev" class="font-semibold">@OtchereDev</a>
+                </p>
+                <p class="text-base text-gray-300 my-5">
+
+                    You can email me directly at oliverotchere4@gmail.com
+                </p>
+                <BackBtn/>
+            </div>
+
+      </div>
+</template>
+
+<script>
+import BackBtn from '@/components/BackBtn'
+export default {
+    components:{BackBtn}
+}
+</script>
+
+<style>
+
+</style>
