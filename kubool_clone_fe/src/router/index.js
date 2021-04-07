8c568182b31727acdc4e4fb299a81695cf4e6a0c
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-07 12:24:27 +0000

    added scroll to top behaviour

--- kubool_clone_fe/src/router/index.js
+++ kubool_clone_fe/src/router/index.js
@@ -42,7 +42,11 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior() {
+    document.getElementById('app').scrollIntoView();
+}
+
 })
 
 export default router
