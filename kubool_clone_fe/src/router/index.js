b3490f0fb58e0caf5fb804a68f9d78516657d6b1
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-13 05:30:25 +0000

    added all route for application

--- kubool_clone_fe/src/router/index.js
+++ kubool_clone_fe/src/router/index.js
@@ -1,3 +1,4 @@
+
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Auth from '../views/Auth.vue'
@@ -5,38 +6,79 @@ import Settings from '../views/Settings.vue'
 import ChangePassword from '../views/ChangePassword.vue'
 import Disclaimer from '../views/Disclaimer.vue'
 import Contact from '../views/Contact.vue'
+import Messages from '../views/Messages.vue'
+import SendMessage from '../views/SendMessage.vue'
 
+import getCurrentUser from '../composibles/getUser'
+
+const requireAuth=async(to,from,next)=>{
+    const {user,error}=await getCurrentUser()
+   
+    if(user.value !==null){
+      next()
+    }else{
+      next({name:'Auth'})
+    }
+}
+
+const requireNoAuth=async(to,from,next)=>{
+  const {user,error}=await getCurrentUser()
+  console.log(user.value)
+  if(user.value ===null){
+    next()
+  }else{
+    next({name:'Home'})
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter:requireAuth
   },
  {
    path:'/auth',
    name:'Auth',
-   component:Auth
+   component:Auth,
+   beforeEnter:requireNoAuth
  },
  {
    path:'/settings',
    name:'Settings',
-   component:Settings
+   component:Settings,
+   beforeEnter:requireAuth
  },
  {
    path:'/settings/change_password',
    name:'Change_password',
-   component:ChangePassword
+   component:ChangePassword,
+   beforeEnter:requireAuth
  },
  {
   path:'/settings/disclaimer',
   name:'Disclaimer',
-  component:Disclaimer
+  component:Disclaimer,
+  beforeEnter:requireAuth
 },
 {
-  path:'/contact',
+  path:'/about',
   name:'Contact',
-  component:Contact
+  component:Contact,
+  beforeEnter:requireAuth
+},
+{
+  path:'/messages',
+  name:'Messages',
+  component:Messages,
+  beforeEnter:requireAuth
+},
+{
+  path:'/:shared_code',
+  name:'SendMessage',
+  component:SendMessage,
+  props:true
 }
 ]
 
