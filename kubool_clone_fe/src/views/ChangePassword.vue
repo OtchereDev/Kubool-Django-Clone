7eb3fd4c9096c8588806b7d0bb8d7400bbd51f6d
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-13 06:15:12 +0000

    added a view for changing password

--- kubool_clone_fe/src/views/ChangePassword.vue
+++ kubool_clone_fe/src/views/ChangePassword.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen bg-custome_bg bg-cover bg-no-repeat py-20 font-custom_font">
-            <div class="mx-auto w-11/12 md:w-5/12 bg-custom_blue py-20 rounded-xl px-16">
+            <div class="mx-auto w-11/12 md:w-8/12 lg:w-5/12 bg-custom_blue py-20 rounded-xl px-8 md:px-10 lg:px-16">
 
                 <h2 class="text-center  font-bold text-gray-50 text-4xl tracking-wide">
                     Change Password
@@ -9,15 +9,33 @@
                 <p class="text-gray-300 text-sm text-center my-6 ">
                     You can change your password from here.
                 </p>
+                <div class="text-red-600 text-center font-semibold my-4 text-sm " v-if="error">
+                    <div v-if="error.validation_error">
+                        <p v-for="obj,index in error.validation_error" :key="index">
+                            {{obj}}
+                        </p>
+                    </div>
+                   
+                    <div v-else-if="error.password">
+                        <p >
+                            {{error.password}}
+                        </p>
+                    </div>
+                    <p v-else>{{error}}</p>
+                </div>
+                <div class="text-green-600 text-center font-semibold my-4 text-sm " v-if="success">
+                    {{success}}
+                </div>
 
-                <form >
+                <form @submit.prevent="handleSubmit" >
                     <div class="my-10">
                         <label for="password" class="block text-gray-200 text-sm my-2">New Password</label>
-                        <input type="password" id="password" placeholder="Enter your password" autocomplete='off' class="block w-full bg-transparent py-3 px-2 placeholder-gray-400 outline-none border-b-2 font-medium text-gray-100 text-lg border-gray-50 tracking-wide focus:border-gray-400 transition-colors duration-500" required>
+                        <input v-model="password" type="password" id="password" placeholder="Enter your password" autocomplete='off' class="block w-full bg-transparent py-3 px-2 placeholder-gray-400 outline-none border-b-2 font-medium text-gray-100 text-lg border-gray-50 tracking-wide focus:border-gray-400 transition-colors duration-500" required>
                     </div>
                     <div class="text-center my-10">
-                        <button style="border-radius:32px;" class="flex w-full rounded-3xl py-4 text-center justify-center items-center bg-custome_bg text-gray-100 text-lg focus:outline-none">
-                            <span class="mr-2 font-medium">Change Password</span><ion-icon class="text-2xl font-medium" name="arrow-forward-outline"></ion-icon>
+                        <button ref="btn" style="border-radius:32px;" class="flex w-full rounded-3xl py-4 text-center justify-center items-center bg-custome_bg text-gray-100 text-lg focus:outline-none">
+                            <span v-if="!requesting" class="mr-2 font-medium">Change Password</span><ion-icon v-if="!requesting" class="text-2xl font-medium" name="arrow-forward-outline"></ion-icon>
+                            <span v-if="requesting">Please wait...</span>
                         </button>
                     </div>
                 </form>
@@ -27,9 +45,43 @@
 </template>
 
 <script>
+import {ref} from 'vue'
 import BackBtn from '@/components/BackBtn.vue'
+import handleChangePassword from '@/composibles/handleChangePassword'
 export default {
-    components:{BackBtn}
+    components:{BackBtn},
+    setup(){
+       
+        const password=ref('')
+
+        const requesting=ref(false)
+        const btn=ref(null)
+
+        const {error,change_password,success}=handleChangePassword()
+
+        const handleSubmit= async()=>{
+            error.value=null
+            if( password.value.length){
+                error.value=null
+
+                requesting.value=true
+                btn.value.style.disabled=true
+                const res= await change_password({password:password.value})
+
+                if(error.value===null){
+                    password.value=null
+                }
+
+                requesting.value=false
+                btn.value.style.disabled=false
+            }else{
+                error.value='Please complete the form'
+            }
+        }
+
+        return {success,password,error,handleSubmit,btn,requesting}
+    
+    }
 }
 </script>
 
