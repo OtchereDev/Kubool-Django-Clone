d5ba94f86705748a22d922f9da032fdb176f4fd4
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-14 05:30:18 +0000

    added a component for single user

---
+++ kubool_clone_fe/src/components/Message.vue
@@ -0,0 +1,32 @@
+<template>
+    <div style="border-color:#5fdee2" class=" my-2 border rounded-lg py-4 px-2 text-gray-100 ">
+        <p>
+            Message:
+        </p>
+        <p>
+            {{message.content}}
+        </p>
+        <p class="my-1">
+            - Anonymous [{{formatDate}}]
+        </p>
+    </div>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+export default {
+    props:['message'],
+    setup(props){
+        const formatDate=computed(()=>{
+            const date= new Date(props.message.created)
+            return `${date.toDateString()} ${date.toLocaleTimeString()} GMT`
+        })
+
+        return {formatDate}
+    }
+}
+</script>
+
+<style>
+
+</style>
