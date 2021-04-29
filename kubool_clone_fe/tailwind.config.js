8f2cb4c3fdc08c8240319cdfbde9f5a7c9dbdeb8
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-29 11:05:02 +0000

    added a tailwind: config

---
+++ kubool_clone_fe/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custome_bg':'linear-gradient(45deg,#00dbde,#fc00ff)'
+      },
+      colors:{
+        'custom_blue':'#250933',
+        'custom_brown':'#212529 '
+      },
+      "fontFamily":{
+        'custom_font':"Poppins"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
