f1e67329510f2da77290480c879757b2438d145f
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-30 09:15:01 +0000

    added url for landing page and removed root path from messaging app

--- kubool_clone/urls.py
+++ kubool_clone/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic.base import TemplateView
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -26,6 +27,7 @@ urlpatterns = [
     path('api/',include('api.urls',namespace='api')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('',include('messaging.urls',namespace='messaging')),
+    path('messaging/',include('messaging.urls',namespace='messaging')),
+    path('',TemplateView.as_view(template_name='index.html')),
    
 ]
